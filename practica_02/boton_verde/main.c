3a91f6c04955f6f3267db5bca77b47e3ba43ffc8
luisUCM <luisyama@ucm.es>
2026-03-04 09:57:53 +0100

    as
    Co-Authored-By: Diego Gonzalez del Castillo Ramirez de Arellano <181630732+diego-glezz@users.noreply.github.com>

---
+++ practica_02/boton_verde/main.c
@@ -0,0 +1,36 @@
+#include <msp430.h>
+
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;
+
+    PM5CTL0 &= ~LOCKLPM5;
+
+    P1SEL0 &= ~BIT0;
+    P1SEL1 &= ~BIT0;
+    
+    P9SEL0 &= ~BIT7;
+    P9SEL1 &= ~BIT7;
+
+    P1SEL0 &= ~BIT1;
+    P1SEL1 &= ~BIT1;
+    P1DIR &= ~BIT1;
+    P1OUT |= BIT1;
+
+    int rojo = 1;
+
+    P1DIR |= BIT0;
+    P9DIR |= BIT7;
+
+    P1OUT |= BIT0;
+    P9OUT |= BIT7;
+
+    while(1) {
+        if (P1IN & BIT1) rojo = (rojo == 1) ? 0 : 1;
+        if (rojo) P1OUT ^= BIT0;
+        P9OUT ^= BIT7;
+
+        __delay_cycles(300000);
+    }
+
+    return 0;
+}
